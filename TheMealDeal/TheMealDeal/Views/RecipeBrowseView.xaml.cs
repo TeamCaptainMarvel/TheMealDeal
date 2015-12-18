f6aca98ac2e08c4dc68fc4caf2431484a49fcac9
Bianca Hinova <bianca.hinova@gmail.com>
2015-12-18 19:27:44 +0200

    browse and favourites as a view

---
+++ TheMealDeal/TheMealDeal/Views/RecipeBrowseView.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TheMealDeal.Views
+{
+    using TheMealDeal.ViewModels;
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class RecipeBrowseView : UserControl
+    {
+        public RecipeBrowseView()
+        {
+            this.InitializeComponent();
+            this.DataContext = new BrowseViewModel();
+        }
+    }
+}
