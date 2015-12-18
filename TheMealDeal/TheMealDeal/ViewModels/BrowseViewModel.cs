e70db998674f7d5dac1a3666fd30da95502a6857
Bianca Hinova <bianca.hinova@gmail.com>
2015-12-18 20:11:21 +0200

    observable collection

--- TheMealDeal/TheMealDeal/ViewModels/BrowseViewModel.cs
+++ TheMealDeal/TheMealDeal/ViewModels/BrowseViewModel.cs
@@ -1,15 +1,34 @@
 ﻿namespace TheMealDeal.ViewModels
 {
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
 
     public class BrowseViewModel
     {
         //3 collections for the 3 types of recipes
-        public IEnumerable<RecipeViewModel> Recipes { get; set; }
+        private ObservableCollection<RecipeViewModel> vegeterianRecipes;
+        private ObservableCollection<RecipeViewModel> veganRecipes;
+        private ObservableCollection<RecipeViewModel> meatRecipes;
+
+        public IEnumerable<RecipeViewModel> VegeterianRecipes
+        {
+            get { return this.vegeterianRecipes; }
+            set { this.vegeterianRecipes = new ObservableCollection<RecipeViewModel>(value); }
+        }
+        public IEnumerable<RecipeViewModel> VeganRecipes
+        {
+            get { return this.veganRecipes; }
+            set { this.veganRecipes = new ObservableCollection<RecipeViewModel>(value); }
+        }
+        public IEnumerable<RecipeViewModel> MeatRecipes
+        {
+            get { return this.meatRecipes; }
+            set { this.meatRecipes = new ObservableCollection<RecipeViewModel>(value); }
+        }
 
         public BrowseViewModel()
         {
-            this.Recipes = new RecipeViewModel[]
+            this.VegeterianRecipes = new RecipeViewModel[]
             {
                 new RecipeViewModel(
                     title: "Sandwitch",
@@ -70,6 +89,57 @@
                     description: "take two slices of bread, put smth on them, eat them",
                     type: "meat",
                     ingredients: "bread, butter, ham, tomato")
+            };
+
+            this.VeganRecipes = new RecipeViewModel[]
+            {
+                new RecipeViewModel(
+                    title: "Sandwitch3",
+                    description: "take two slices of bread, put smth on them, eat them",
+                    type: "meat",
+                    ingredients:"bread, butter, ham, tomato",
+                    imageUrl:"http://www.solutionstalk.com/home/wp-content/uploads/bigstock-club-sandwich-clubhouse-Sand-26008286.jpg"),
+                new RecipeViewModel(
+                    title: "Sandwitch4",
+                    description: "take two slices of bread, put smth on them, eat them",
+                    type: "meat",
+                    ingredients: "bread, butter, ham, tomato",
+                    imageUrl:"http://globe-views.com/dcim/dreams/sandwich/sandwich-01.jpg"),
+                new RecipeViewModel(
+                    title: "Sandwitch5",
+                    description: "take two slices of bread, put smth on them, eat them",
+                    type: "meat",
+                    ingredients: "bread, butter, ham, tomato"),
+                new RecipeViewModel(
+                    title: "Sandwitch6",
+                    description: "take two slices of bread, put smth on them, eat them",
+                    type: "meat",
+                    ingredients: "bread, butter, ham, tomato"),
+            };
+            this.MeatRecipes = new RecipeViewModel[]
+            {
+                new RecipeViewModel(
+                    title: "Sandwitch3",
+                    description: "take two slices of bread, put smth on them, eat them",
+                    type: "meat",
+                    ingredients:"bread, butter, ham, tomato",
+                    imageUrl:"http://www.solutionstalk.com/home/wp-content/uploads/bigstock-club-sandwich-clubhouse-Sand-26008286.jpg"),
+                new RecipeViewModel(
+                    title: "Sandwitch4",
+                    description: "take two slices of bread, put smth on them, eat them",
+                    type: "meat",
+                    ingredients: "bread, butter, ham, tomato",
+                    imageUrl:"http://globe-views.com/dcim/dreams/sandwich/sandwich-01.jpg"),
+                new RecipeViewModel(
+                    title: "Sandwitch5",
+                    description: "take two slices of bread, put smth on them, eat them",
+                    type: "meat",
+                    ingredients: "bread, butter, ham, tomato"),
+                new RecipeViewModel(
+                    title: "Sandwitch6",
+                    description: "take two slices of bread, put smth on them, eat them",
+                    type: "meat",
+                    ingredients: "bread, butter, ham, tomato"),
             };
         }
     }
