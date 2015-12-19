8d8dd2bb806b886d6d0b32b0c5778ebb2e5a5be3
Bianca Hinova <bianca.hinova@outlook.com>
2015-12-19 21:45:23 +0200

    switch comps

--- TheMealDelaWebAPI/TheMealDealWebApi/Data/TheMealDeal.Data/TheMealDealDbContext.cs
+++ TheMealDelaWebAPI/TheMealDealWebApi/Data/TheMealDeal.Data/TheMealDealDbContext.cs
@@ -2,13 +2,15 @@
 {
     using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
-
+    using System.Data.Entity;
     public class TheMealDealDbContext : IdentityDbContext<User>
     {
         public TheMealDealDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+
+        public virtual IDbSet<Recipe> Recipes { get; set; }
 
         public static TheMealDealDbContext Create()
         {
